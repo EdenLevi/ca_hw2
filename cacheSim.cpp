7a0c9177e930ca3272ce08436946406dd3d35647
w0lfl0w <pazwolfwolfpaz@gmail.com>
2022-12-21 22:14:08 +0200

    cleaning before we submit

--- cacheSim.cpp
+++ cacheSim.cpp
@@ -23,7 +23,8 @@ public:
     unsigned int address;
     unsigned int tag;
     unsigned int way;
-    block(bool validBit, bool dirtyBit, unsigned int address, unsigned int tag, unsigned int way) : validBit(validBit), dirtyBit(dirtyBit), address(address), tag(tag), way(way) {};
+    block(bool validBit, bool dirtyBit, unsigned int address, unsigned int tag, unsigned int way) :validBit(validBit),
+                                                            dirtyBit(dirtyBit), address(address), tag(tag), way(way) {};
 };
 
 class cache {
@@ -42,8 +43,7 @@ public:
     int BSize;
     int L1Ways;
     int L2Ways;
-    int L1BlocksNum;
-    int L2BlocksNum;
+
 
     int L1tagSize;
     int L2tagSize;
@@ -51,9 +51,11 @@ public:
     int L2SetsNum;
 
 
-    cache(int L1Cyc, int L2Cyc, int MemCyc, int BSize, int L1Ways,  int L2Ways, int L1BlocksNum, int L2BlocksNum, int L1tagSize, int L2tagSize, int L1SetsNum, int L2SetsNum)
-    : L1(nullptr), L2(nullptr), L1Cyc(L1Cyc), L2Cyc(L2Cyc), MemCyc(MemCyc), totalTime(0), L1misses(0), L1access(0), L2misses(0), L2access(0), BSize(BSize), L1Ways(L1Ways), L2Ways(L2Ways),
-    L1BlocksNum(L1BlocksNum), L2BlocksNum(L2BlocksNum),  L1tagSize(L1tagSize), L2tagSize(L2tagSize), L1SetsNum(L1SetsNum), L2SetsNum(L2SetsNum)
+    cache(int L1Cyc, int L2Cyc, int MemCyc, int BSize, int L1Ways,  int L2Ways,
+          int L1tagSize, int L2tagSize, int L1SetsNum, int L2SetsNum)
+    : L1(nullptr), L2(nullptr), L1Cyc(L1Cyc), L2Cyc(L2Cyc), MemCyc(MemCyc), totalTime(0), L1misses(0), L1access(0),
+      L2misses(0), L2access(0), BSize(BSize), L1Ways(L1Ways), L2Ways(L2Ways), L1tagSize(L1tagSize),
+      L2tagSize(L2tagSize), L1SetsNum(L1SetsNum), L2SetsNum(L2SetsNum)
     {
         if(L1SetsNum > 0) L1 = new std::list<block>[L1SetsNum];
 
@@ -76,41 +78,15 @@ public:
 
     unsigned int getTagFromAddress(long unsigned int address, bool isL1) {
         int tagSize = isL1 ? L1tagSize : L2tagSize;
-        /*(int tag = (address >> BSize) >> (pow(2, BSize) - );
-
-        int offset_size_bits = BSize; // BlockSizeBits
-        int Size, Assoc, BlockSizeBits;
-        if(isL1) {
-            Size =
-        }
-        int set_field_size_bits = Size - Assoc - BlockSizeBits;
-
-        int tag = (address >> offset_size_bits) >> set_field_size_bits;*/
-        cout << tagSize << endl;
         return (address >> (32-tagSize));
     }
 
     unsigned int getSetFromAddress(long unsigned int address, bool isL1) {
 
-        cout << "I am trying to get set from address: " << address << endl;
         int set = address >> BSize;
         int mask = isL1 ? L1SetsNum : L2SetsNum;
         set = set % mask;
-        cout << "The set I got is: " << set << endl;
         return set;
-
-
-        /*int andByBits = log2(isL1 ? L1SetsNum : L2SetsNum);
-        if(andByBits == 0) return 0; // if there is no Set Bit
-        int set = address >> (BSize);
-        int mask = andByBits - 1;
-        return (set & mask);*/
-
-
-
-        //int bits = isL1 ? log2(L1SetsNum) : log2(L2SetsNum);
-        //return ((address >> BSize) % (1 << bits));
-
     }
 
     void addBlock(unsigned int address, unsigned int tag, unsigned int set, bool isL1,bool isWrite) {
@@ -120,7 +96,6 @@ public:
         /// if there is any empty space, find the lowest way that is empty
         for (std::list<block>::iterator it = blockList[set].begin(); it != blockList[set].end(); it++) {
             if(!it->validBit) {
-                cout << "adding block to empty way_" << it->way << " in " << (isL1 ? "L1" : "L2") << endl;
                 it->address = address;
                 it->tag = tag;
                 it->validBit = true;
@@ -131,11 +106,9 @@ public:
 
         /// if there is no empty space, remove the LRU (last block in list) and add it to the front
         std::list<block>::iterator it = --(blockList[set].end());
-        cout << "writing block over way_" << it->way << endl;
 
         if(!isL1) { // if we are removing from L2, make sure block is removed from L1 (if exists there)
             removeBlock(getTagFromAddress(it->address, true), getSetFromAddress(it->address, true), true);
-
         }
 
         else{ // we in L1, if block has
@@ -159,34 +132,24 @@ public:
         unsigned int tagL2 = getTagFromAddress(address, false);
         unsigned int setL2 = getSetFromAddress(address, false);
 
-        cout << "tagL1 " << tagL1 << endl;
-        cout << "setL1 " << setL1 << endl;
-
         L1access++;
         totalTime += L1Cyc;
         if(isBlockInCache(tagL1, setL1, true)) { // if hit in L1
-            cout << "hit L1" << endl;
             useBlock(tagL1, setL1, true, false);
         }
         else { // if miss on L1
-            cout << "miss L1" << endl;
             L1misses++;
             L2access++;
             totalTime += L2Cyc;
 
-            cout << "tagL2 " << tagL2 << endl;
-            cout << "setL2 " << setL2 << endl;
 
             if(isBlockInCache(tagL2, setL2, false)) { // if hit in L2
-
-                cout << "hit L2" << endl;
 
                 useBlock(tagL2, setL2, false,false);
                 addBlock(address, tagL1, setL1, true, false); // bring to L1 also
                 /// maybe need another L1 cycle? for bringing from L2 to L1
             }
             else { // if miss on L2
-                cout << "miss L2" << endl;
                 L2misses++;
                 totalTime += MemCyc;
                 addBlock(address, tagL2, setL2, false, false); // bring block from mem to L2
@@ -203,35 +166,27 @@ public:
         unsigned int tagL2 = getTagFromAddress(address, false);
         unsigned int setL2 = getSetFromAddress(address, false);
 
-        cout << "tagL1 " << tagL1 << endl;
-        cout << "setL1 " << setL1 << endl;
-
         L1access++;
         totalTime += L1Cyc;
         if(isBlockInCache(tagL1, setL1, true)) { // if hit in L1
             useBlock(tagL1, setL1, true, true);
-            cout << "hit L1" << endl;
         }
         else { // if miss on L1
-
-            cout << "miss L1" << endl;
 
             L1misses++;
             L2access++;
             totalTime += L2Cyc;
             if(isBlockInCache(tagL2, setL2, false)) { // if hit in L2
-                cout << "hit L2" << endl;
-
                 useBlock(tagL2, setL2, false, true);
                 if(writeAlloc) addBlock(address, tagL1, setL1, true, true); // bring to L1 also
             }
             else { // if miss on L2
-                cout << "miss L2" << endl;
-
                 L2misses++;
                 totalTime += MemCyc;
-                if(writeAlloc) addBlock(address, tagL2, setL2, false, true); // bring block from mem to L2
-                if(writeAlloc) addBlock(address, tagL1, setL1, true, true); // bring block from mem to L1
+                // bring block from mem to L2
+                if(writeAlloc) addBlock(address, tagL2, setL2, false, true);
+                // bring block from mem to L1
+                if(writeAlloc) addBlock(address, tagL1, setL1, true, true);
             }
         }
     }
@@ -270,21 +225,17 @@ public:
 
         for (std::list<block>::iterator it = blockList[set].begin(); it != blockList[set].end(); it++) {
             if(it->validBit && it->tag == tag) { // found the block we wanted
-                if((it->dirtyBit) && isL1 ) { // if it has a dirty bit we need to update the block in the lower level
+                /*if((it->dirtyBit) && isL1 ) { // if it has a dirty bit we need to update the block in the lower level
                     /// use the block in L2
-                    cout << "where is sara corner, where is she? \n" ;
-                }
-
+                } */ /// we were told we dont need to "use" a block in L2 if we update it due to deleting a dirty block
+                     /// from L1
                 it->tag = 0;
                 it->address = 0;
                 it->validBit = false;
                 return;
             }
         }
-
     }
-
-
 };
 
 
@@ -337,50 +288,22 @@ int main(int argc, char **argv) {
         }
     }
 
-    cout << "we are here" << endl;
-
     int L1Ways = pow(2, L1Assoc); // assoc = log2 (# of ways)
     int L2Ways = pow(2, L2Assoc); // assoc = log2 (# of ways)
-    // sets = blocks / ways
-    // blocks = cache_size / block_size
-    cout << "got " << L1Ways << " L1Ways" << endl;
-    cout << "got " << L2Ways << " L2Ways" << endl;
+
 
     int L1BlocksNum = pow(2, L1Size) / pow(2, BSize);
     int L2BlocksNum = pow(2, L2Size) / pow(2, BSize);
-    cout << "got " << L1BlocksNum << " L1BlocksNum" << endl;
-    cout << "got " << L2BlocksNum << " L2BlocksNum" << endl;
 
     int L1SetsNum = L1BlocksNum / L1Ways;
     int L2SetsNum = L2BlocksNum / L2Ways;
-    cout << "got " << L1SetsNum << " L1SetsNum" << endl;
-    cout << "got " << L2SetsNum << " L2SetsNum" << endl;
-
-   // int L1SetsNum = L1Size - L1Assoc - BSize;
-   // int L2SetsNum = L2Size - L2Assoc - BSize;
 
     int L1tagSize = 32 - ((log2(L1SetsNum) + BSize)) ;
     int L2tagSize = 32 - ((log2(L2SetsNum) + BSize)) ;
-    cout << "got " << L1tagSize << " L1tagSize" << endl;
-    cout << "got " << L2tagSize << " L2tagSize" << endl;
-    //int L1tagSize = 32 - BSize - L1SetsNum;
-    //int L2tagSize = 32 - BSize - L2SetsNum;
 
-    //int numOfBlocksL1_inBits = L1Size - BSize;
-    //int numOfSetsL1_inBits = numOfBlocksL1_inBits - L1Assoc;
-    //int numOfBlocksL2_inBits = L2Size - BSize;
-    //int numOfSetsL2_inBits = numOfBlocksL2_inBits - L1Assoc;
+    cache Cache(L1Cyc, L2Cyc, MemCyc, BSize, L1Ways, L2Ways, L1tagSize, L2tagSize, L1SetsNum, L2SetsNum);
 
-   // L1tagSize = 32 - numOfSetsL1_inBits - BSize;
-   // L2tagSize = 32 - numOfSetsL2_inBits - BSize;
-
-
-    cout << "block offset size: " << BSize << ", L1 set size: " << log2(L1SetsNum) << ", L2 set size: " << log2(L2SetsNum) << ", L1 tag size:" << L1tagSize << ", L2 tag size:" << L2tagSize << endl;
-
-    cout << "L1Ways: " << L1Ways << ", L2Ways: " << L2Ways << ", L2SetsNum: " << L1SetsNum << ", L2SetsNum: " << L2SetsNum << endl;
-
-    cache Cache(L1Cyc, L2Cyc, MemCyc, BSize, L1Ways, L2Ways, L1BlocksNum, L2BlocksNum, L1tagSize, L2tagSize, L1SetsNum, L2SetsNum);
-
+    bool debugFlag = false;
     int cmdCounter = 0;
     while (getline(file, line)) {
 
@@ -394,55 +317,51 @@ int main(int argc, char **argv) {
         }
 
         // DEBUG - remove this line
-        cout << "operation: " << operation;
+        if(debugFlag) cout << "operation: " << operation;
 
         string cutAddress = address.substr(2); // Removing the "0x" part of the address
 
         // DEBUG - remove this line
-        cout << ", address (hex)" << cutAddress;
+        if (debugFlag) {cout << ", address (hex)" << cutAddress;}
 
         unsigned long int num = 0;
         num = strtoul(cutAddress.c_str(), NULL, 16);
 
         // DEBUG - remove this line
-        cout << " (dec) " << num << endl;
-        if(num == 131072){
-            cout << "debug stuff" << endl;
-        }
+        if (debugFlag) {cout << " (dec) " << num << endl;}
         if(operation == 'r') Cache.read(num);
         else if(operation == 'w') Cache.write(num, WrAlloc);
         cmdCounter++;
 
 
         // DEBUG STUFF
-        cout << "L1:" << endl;
-        for(int i = 0; i < L1SetsNum; i++){
-            for (std::list<block>::iterator it = Cache.L1[i].begin(); it != Cache.L1[i].end(); it++) {
-                if(it->validBit){
-                    cout << it->dirtyBit << " " ;
+        if (debugFlag) {
+            cout << "L1:" << endl;
+            for (int i = 0; i < L1SetsNum; i++) {
+                for (std::list<block>::iterator it = Cache.L1[i].begin(); it != Cache.L1[i].end(); it++) {
+                    if (it->validBit) {
+                        cout << it->dirtyBit << " ";
+                    } else {
+                        cout << "INVALID" << " ";
+                    }
                 }
-                else{
-                    cout << "INVALID" << " " ;
+                cout << endl;
+            }
+            cout << endl;
+
+            cout << "L2:" << endl;
+            for (int i = 0; i < L2SetsNum; i++) {
+                for (std::list<block>::iterator it = Cache.L2[i].begin(); it != Cache.L2[i].end(); it++) {
+                    if (it->validBit) {
+                        cout << it->dirtyBit << " ";
+                    } else {
+                        cout << "INVALID" << " ";
+                    }
                 }
+                cout << endl;
             }
             cout << endl;
         }
-        cout << endl;
-
-        cout << "L2:" << endl;
-        for(int i = 0; i < L2SetsNum; i++){
-            for (std::list<block>::iterator it = Cache.L2[i].begin(); it != Cache.L2[i].end(); it++) {
-                if(it->validBit){
-                    cout << it->dirtyBit << " " ;
-                }
-                else{
-                    cout << "INVALID" << " " ;
-                }
-            }
-            cout << endl;
-        }
-        cout << endl;
-
     }
 
     double L1MissRate = (double)Cache.L1misses/Cache.L1access;
